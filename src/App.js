1ff93fb9ed6a08920b481d45cf31a53ef08f4f60
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2018-10-22 22:10:31 +0100

    Completed the module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import './App.css'
 
+// Components
+import Quiz from './Quiz';
+
 class App extends Component {
   render() {
     return (
-      <div />
+      <Quiz />
     )
   }
 }
