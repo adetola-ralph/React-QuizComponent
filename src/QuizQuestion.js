5996cf95f53ce7fbdbad544730534faadd5e5b60
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2018-10-23 06:13:18 +0100

    Show incorrect messages based on answer chosen

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,10 +4,25 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      incorrectAnswer: false,
+    };
+  }
+
   handleClick(buttonText) {
     const { showNextQuestionHandler } = this.props;
     if (buttonText === this.props.quiz_question.answer) {
       showNextQuestionHandler();
+      this.setState({
+        incorrectAnswer: false,
+      });
+    } else {
+      this.setState({
+        incorrectAnswer: true,
+      });
     }
   }
 
@@ -26,6 +41,7 @@ class QuizQuestion extends Component {
             }
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null}
       </main>
     );
   }
