64cd8dabbf5f57fc81295473b94c1a2625c6eda0
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2018-10-23 06:44:40 +0100

    Add more trivia questions

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
 class QuizEnd extends Component {
-  handleResetClick() {
+  handleResetClick(event) {
+    event.preventDefault();
     this.props.resetClickHandler()
   }
 
@@ -9,7 +10,7 @@ class QuizEnd extends Component {
     return (
       <div>
         <p>Thanks for playing!</p>
-        <a onClick={this.handleResetClick.bind(this)}>Reset Quiz</a>
+        <a href="" onClick={this.handleResetClick.bind(this)}>Reset Quiz</a>
       </div>
     );
   }
